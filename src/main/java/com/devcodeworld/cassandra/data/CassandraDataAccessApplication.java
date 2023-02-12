0e3e27256253dd12b29bb813325645fe2326ebee
pattabhia <shivaram2004@gmail.com>
2023-02-12 13:19:51 +0530

    Initial Commit

---
+++ src/main/java/com/devcodeworld/cassandra/data/CassandraDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.devcodeworld.cassandra.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraDataAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CassandraDataAccessApplication.class, args);
+    }
+
+}
